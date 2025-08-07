0f96aea0bfe2b899deccb6bfda4a104583f0e019
SergioAndresG <sergiogarcia3421gmail.com>
2025-08-07 13:30:22 -0500

    feat(api): reorganizar clase UsuarioGenerador en usuario_schemas.py

--- SCHEMAS/usuario_schemas.py
+++ SCHEMAS/usuario_schemas.py
@@ -1,6 +1,17 @@
 from pydantic import BaseModel
 from enum import Enum
+from typing import Optional
 
 class Rol(str, Enum):
     INSTRUCTOR = "INSTRUCTOR"
     ADMINISTRADOR = "ADMINISTRADOR"
+
+class UsuarioGenerador(BaseModel):
+    id: Optional[int] = None
+    nombre: str
+    apellidos: str
+    correo: str
+    rol: Rol
+
+    class Config:
+        orm_mode = True
