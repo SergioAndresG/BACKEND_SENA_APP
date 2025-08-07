99a022dba3ab2fd1f0abe499491e6dee81b640de
SergioAndresG <sergiogarcia3421gmail.com>
2025-08-07 13:29:35 -0500

    feat(api): crear modelo de Usuarios con relación a archivos generados

---
+++ MODELS/a_usuarios.py
@@ -0,0 +1,17 @@
+from SCHEMAS.usuario_schemas import Rol
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Enum
+from sqlalchemy.orm import relationship
+from connection import base
+
+
+class Usuarios(base): 
+    __tablename__ = "Usuarios"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(255), nullable=False)
+    apellidos = Column(String(255), nullable=False)
+    correo = Column(String(255), nullable=False)
+    rol = Column(Enum(Rol), nullable=False)
+    contraseña = Column(String(255), nullable=True)
+
+    # Relación con la tabla de archivos
+    archivos_generados = relationship("ArchivoExcel", back_populates="usuario")
