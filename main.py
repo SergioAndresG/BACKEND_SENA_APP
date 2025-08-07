e5fdb1225f199b10b1d81245242985dcc825fb05
SergioAndresG <sergiogarcia3421gmail.com>
2025-08-07 13:28:04 -0500

    feat(api): Incluir enrutador para aprendices en la aplicación principal

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from connection import base, crear
 from EDNPOINTS.fichas import router_tokens
 from EDNPOINTS.formatos import router_format
+from EDNPOINTS.aprendices import router_aprendices
+
+from MODELS.a_usuarios import Usuarios
+from MODELS.archivo_excel import ArchivoExcel
 
 app = FastAPI(title="SENA - Procesador de Fichas")
 app.include_router(router_tokens)
 app.include_router(router_format)
-
-
-
+app.include_router(router_aprendices)
 
 app.add_middleware(
     CORSMiddleware,
