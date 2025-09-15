0daf55abcfd10ad97feb87a6b188db963c2aafda
SergioAndresG <sergiogarcia3421gmail.com>
2025-09-15 18:55:12 -0500

    feat(aprendiz): agregue los campos firma, ultima_actualizacion y editado al modelo

--- MODELS/aprendices.py
+++ MODELS/aprendices.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.orm import relationship
-
+from datetime import datetime
 from connection import base
 
 class Aprendiz(base):
@@ -8,14 +9,18 @@ class Aprendiz(base):
     id_aprendiz = Column(Integer, primary_key=True, autoincrement=True)
     documento = Column(String(20), nullable=False, unique=True) 
     nombre = Column(String(100), nullable=False)    
-    apellido = Column(String(100), nullable=False)  
+    apellido = Column(String(100), nullable=False)
     correo = Column(String(100), nullable=False)    
     celular = Column(String(20), nullable=False)
     direccion = Column(String(200), nullable=True)
     departamento = Column(String(50), nullable=True)
     municipio = Column(String(50), nullable=True)
-    tipo_documento = Column(String(10), nullable=True)  
+    tipo_documento = Column(String(10), nullable=True)
     estado = Column(String(50), nullable=True)
+    firma = Column(LONGTEXT, nullable=True)
+    ultima_actualizacion = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    editado = Column(Boolean, default=False)
+
 
 
     # Clave foránea para ficha
