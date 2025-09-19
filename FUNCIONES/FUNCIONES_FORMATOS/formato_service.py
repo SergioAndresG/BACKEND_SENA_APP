2c2b7d0dea0fdead6de979d8560b94d52dd13472
SergioAndresG <sergiogarcia3421gmail.com>
2025-09-18 22:02:01 -0500

    feat(formato_service): mejorar la generación de formatos grupales y corregir la asignación de datos

--- FUNCIONES/FUNCIONES_FORMATOS/formato_service.py
+++ FUNCIONES/FUNCIONES_FORMATOS/formato_service.py
@@ -17,6 +17,10 @@ from openpyxl import Workbook, load_workbook
 from openpyxl.drawing.image import Image as OpenpyxlImage
 from copy import deepcopy
 from io import BytesIO
+from MODELS import ArchivoExcel, Ficha
+from fastapi import Depends
+
+
 
 class FormatoService:
     def __init__(self,base_path = "archivos_exportados"):
@@ -203,10 +207,10 @@ class FormatoService:
     
 
     def _llenar_F165_grupal(self,wb,ficha,aprendices,imagenes_procesadas,request,usuario_gene,informacion_adicional):
+
         fecha_inicio = ficha.fecha_inicio.strftime("%d-%m-%Y") if ficha.fecha_inicio else "N/A"
         fecha_fin = ficha.fecha_fin.strftime("%d-%m-%Y") if ficha.fecha_fin else "N/A"
         fecha_actual = datetime.now().strftime("%d-%m-%Y")
-
 
         hoja = wb["Selección formato - Grupal"]
         hoja["E8"] = "x"
@@ -230,7 +234,7 @@ class FormatoService:
 
         hoja["T11"] = request.ficha  # Número de ficha
         
-        hoja["H11"] = f"Técnico {ficha.programa}"
+        hoja["H11"] = f"{ficha.programa}"
 
         nombre_completo_instructor = f"{usuario_gene.nombre} {usuario_gene.apellidos}"
 
@@ -242,7 +246,6 @@ class FormatoService:
         hoja["J13"] = informacion_adicional.trimestre
         hoja["J14"] = informacion_adicional.jornada
         hoja["R12"] = informacion_adicional.modalidad_formacion
-        hoja["H11"] = informacion_adicional.nivel_formacion
         hoja["E14"] = informacion_adicional.fecha_inicio_etapa_productiva
         
 
@@ -421,9 +424,12 @@ class FormatoService:
 
         print(f"Worksheets en wb: {[ws.title for ws in wb.worksheets]}")
         print(f"Hoja activa: {wb.active.title}")
+
+        ficha = db.query(Ficha).filter(Ficha.numero_ficha == request.ficha).first()
         
         # Verificar que no hay celdas problemáticas
         hoja = wb["Selección formato - Grupal"]
+        hoja["H11"] = f"{informacion_adicional.nivel_formacion} - {ficha.programa}"
         print(f"Max row: {hoja.max_row}, Max col: {hoja.max_column}")
 
         try:
