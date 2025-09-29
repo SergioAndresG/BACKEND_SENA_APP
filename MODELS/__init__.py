416560e559a53f91f8f7813e2baede19859462a4
SergioAndresG <sergiogarcia3421gmail.com>
2025-09-29 12:55:25 -0500

    feat(token_blacklist.py): agregar modelo TokenBlacklist para gestionar tokens expirados

--- MODELS/__init__.py
+++ MODELS/__init__.py
@@ -3,3 +3,4 @@ from .ficha import Ficha
 from .a_usuarios import Usuarios
 from .ficha_maestro import FichaMaestro
 from .archivo_excel import ArchivoExcel
+from .token_blacklist import TokenBlacklist
